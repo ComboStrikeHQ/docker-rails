871eb6ce6969b41978517f5f4c8923f4acfdae2d
Guilherme Goettems Schneider <guigs81@gmail.com>
2017-07-20 17:53:16 -0300

    Don't need email for docker login anymore

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 . VERSION
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker tag docker-rails-onbuild $DOCKER_TAG:latest
 docker push $DOCKER_TAG:latest
 
