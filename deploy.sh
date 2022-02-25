6cbd1f229455086288f6e49065da708391520940
Helge Rausch <helge@rausch.io>
2022-02-25 12:03:24 +0100

    Add support for Ruby 2.7 (#39)
    * Update Ruby to 2.7.3
    * Remove old node version

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker tag docker-rails-onbuild $DOCKER_TAG:latest
 docker push $DOCKER_TAG:latest
-docker tag docker-rails-onbuild $DOCKER_TAG:ruby-2.5
-docker push $DOCKER_TAG:ruby-2.5
+docker tag docker-rails-onbuild $DOCKER_TAG:ruby-2.7
+docker push $DOCKER_TAG:ruby-2.7
 
 if ! docker pull $DOCKER_TAG:$VERSION; then
   echo "Releasing new version: $VERSION"
